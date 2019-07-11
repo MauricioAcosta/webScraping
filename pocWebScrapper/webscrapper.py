f6fae7383ec94143e92b92ba5cd35e48f28504c5
MauricioAcosta <andres.taamap@gmail.com>
2019-07-11 18:02:46 -0500

    se realiza prueba de concepto para traer titulo, imagen y link de la sección de noticias de proadmintierra, tambien, se concatenan los elementos para la creación de una lista con formato JSON

--- pocWebScrapper/webscrapper.py
+++ pocWebScrapper/webscrapper.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import json
 
 notices=[]
+titlenotices=[]
+hrefnotices=[]
 for i in range(1,4):
         page_link = 'https://www.proadmintierra.info/category/blog/noticias/page/'+str(i)+'/'
         page_response = requests.get(page_link, timeout=5)
@@ -11,13 +14,17 @@ for i in range(1,4):
         div = page_content.find_all('div', {"class" : 'meta-info-container'})
         #aux=''.join(div)
         notices.append(div)
+        for title in page_content.find_all('h3', {"class" : 'entry-title td-module-title'}):
+            if title.text not in titlenotices:
+                hrefnotices.append(str(title.find('a')))
+                titlenotices.append(title.text)
+                
+                 
 allnotices=[]
 for pageNotices in notices:
     for onenotice in pageNotices:
         allnotices.append(str(onenotice))
-#print(allnotices)
-textTitle=[]
-#urlNotice=[]
+
 urlsImage= []
 for i in range(len(allnotices)):
     aux=allnotices[i].split()
@@ -25,15 +32,18 @@ for i in range(len(allnotices)):
         if re.match('src*', item) != None:
             urlsImage.append(item)
 
+hreftitle=[]
+for i in hrefnotices:
+    aux=i.split()
+    for item in aux:
+        if re.match('href*', item) != None:
+            hreftitle.append(item)
+listjson=[]
+for i in range(len(hrefnotices)):
+    jsonreturn = {'title'+str(i):titlenotices[i], 'image'+str(i):urlsImage[i], 'urlNotice'+str(i):hreftitle[i]}
+    listjson.append(jsonreturn)
+
 # =============================================================================
-# for i in range(len(allnotices)):
-#     aux=allnotices[i].split()
-#     for item in aux:
-#         if re.match('href="https://www.proadmintierra.info/*', item) != None:
-#             urlNotice.append(item)
+# for i in listjson:
+#     print(i)
 # =============================================================================
-            
-for i in range(len(allnotices)):
-    aux=allnotices[0]
-    result=re.match('title="\*\"', '<h3><a title="GeoPython 2019_Suiza">GeoPython 2019_Suiza"</a></h3>')
-    print(result)
